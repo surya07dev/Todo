8bdda4640629addc6a586e363a14963ec76a8dd0
Brian Surya <briansurya@Administrators-MacBook-Air-4.local>
2022-12-30 10:54:43 +0530

    add: functionality of add_card

--- lib/app/modules/home/controller.dart
+++ lib/app/modules/home/controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:todo/app/data/models/task.dart';
 import 'package:todo/app/data/services/storage/repository.dart';
@@ -6,6 +7,9 @@ class HomeController extends GetxController {
   TaskRepository taskRepository;
   HomeController({required this.taskRepository});
 
+  final formKey = GlobalKey<FormState>();
+  final chipIndex = 0.obs;
+  final editCtrl = TextEditingController();
   final tasks = <Task>[].obs;
 
   @override
@@ -13,5 +17,17 @@ class HomeController extends GetxController {
     super.onInit();
     tasks.assignAll(taskRepository.readtasks());
     ever(tasks, (_) => taskRepository.writeTasks(tasks));
+  }
+
+  void changeChipIndex(int value) {
+    chipIndex.value = value;
+  }
+
+  bool addTask(Task task) {
+    if (tasks.contains(task)) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
--- lib/app/modules/home/widgets/add_card.dart
+++ lib/app/modules/home/widgets/add_card.dart
@@ -1,13 +1,15 @@
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:todo/app/core/utils/extensions.dart';
+import 'package:todo/app/data/models/task.dart';
 import 'package:todo/app/modules/home/controller.dart';
 import 'package:todo/app/widgets/icons.dart';
 
 class AddCard extends StatelessWidget {
   final homeCtrl = Get.find<HomeController>();
-AddCard({super.key});
+  AddCard({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +20,85 @@ AddCard({super.key});
       height: squareWidth / 2,
       margin: EdgeInsets.all(3.0.wp),
       child: InkWell(
-        onTap: () {},
+        onTap: () async {
+          await Get.defaultDialog(
+            titlePadding: EdgeInsets.symmetric(vertical: 5.0.wp),
+            radius: 15,
+            title: "New Task",
+            content: Form(
+              key: homeCtrl.formKey,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 3.0.wp),
+                    child: TextFormField(
+                      controller: homeCtrl.editCtrl,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Title",
+                      ),
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return "Please enter your task title";
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 5.0.wp),
+                    child: Wrap(
+                      spacing: 2.0,
+                      children: icons
+                          .map(
+                            (e) => Obx(
+                              (() {
+                                final index = icons.indexOf(e);
+                                return ChoiceChip(
+                                  selectedColor: Colors.grey[200],
+                                  pressElevation: 5,
+                                  backgroundColor: Colors.white,
+                                  label: e,
+                                  selected: homeCtrl.chipIndex.value == index,
+                                  onSelected: (bool selected) {
+                                    homeCtrl.chipIndex.value =
+                                        selected ? index : 0;
+                                  },
+                                );
+                              }),
+                            ),
+                          )
+                          .toList(),
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      minimumSize: const Size(150, 40),
+                    ),
+                    onPressed: () {
+                      if (homeCtrl.formKey.currentState!.validate()) {
+                        int icon =
+                            icons[homeCtrl.chipIndex.value].icon!.codePoint;
+                        String color =
+                            icons[homeCtrl.chipIndex.value].color!.toString();
+                        var task = Task(
+                            title: homeCtrl.editCtrl.text,
+                            icon: icon,
+                            color: color,);
+                            Get.back();
+                            homeCtrl.addTask(task) ? EasyLoading.showSuccess("Create Success") : EasyLoading.showError("Duplicated Task");
+                      }
+                    },
+                    child: const Text("Confirm"),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
         child: DottedBorder(
           color: Colors.grey[400]!,
           dashPattern: const [8, 4],
